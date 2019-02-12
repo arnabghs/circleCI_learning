1da462356611fe2387f2a6865650b655ffb72fdd
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2019-02-12 19:33:36 +0530

    added one passing and one failing test

--- test/unit_test.js
+++ test/unit_test.js
@@ -1,17 +1,30 @@
-const assert = require('assert');
+const assert = require("assert");
 
-const add = function (a, b) {
-	return a + b;
-}
+const add = function(a, b) {
+  return a + b;
+};
 
-const sub = function (a, b) {
-	return a - b;
-}
+const sub = function(a, b) {
+  return a - b;
+};
 
-describe('add', () => {
-	it(' return sum', () => {
-		let actualOutput = add(3, 3);
-		let expectedOutput = 6;
-		assert.deepEqual(actualOutput, expectedOutput);
-	});
+describe("add", () => {
+  it("pass test", () => {
+    let actualOutput = add(3, 3);
+    let expectedOutput = 6;
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+  it("fail test", () => {
+    let actualOutput = add(3, 3);
+    let expectedOutput = 5;
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+});
+
+describe("sub", () => {
+  it("pass test", () => {
+    let actualOutput = sub(3, 1);
+    let expectedOutput = 2;
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
 });
